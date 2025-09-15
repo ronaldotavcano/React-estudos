564391b2b258babf6b75ee1c16273098c82f866c
Ronaldo T Cano <ronaldotavarescano@gmail.com>
2025-09-15 15:06:28 -0300

    Usando css no react

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import "./global.css" // sempre no topo pra nn resetar outras configs
 import {Button} from "./components/button"
+import "./styles.css"
 
 // Todo componente tem que começar com letra maiuscula
 export function App(){
   return (
-    <div>
+    <div className="container">
       <Button name="Criar" onClick={() => alert("Criar")}/>
-      <Button name="Editar"/>
-      <Button name="Remover"/>
+      <Button name="Editar" onClick={() => alert("Editar")}/>
+      <Button name="Remover" onClick={() => alert("Remover")}/>
       
     </div>
   )
